6f5789717ff46a5ee4e5e645f08b9ffc4d007549
aiChenK <aichenk@qq.com>
2022-03-07 20:06:18 +0800

    chore: 去除无用文件

--- restart.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-#默认进入的是登录用户的目录
-cd /aichenk/service/prototype
-tar -xzvf prototype.tar.gz
-#remove conf of dev
-rm -rf conf/app.conf
-cp conf/app.conf.bak conf/app.conf
-supervisorctl restart prototype
